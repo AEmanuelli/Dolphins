4f22b7f8304837584c1ba6b414f1ac68490ac015
AEmanuelli <accent.cartilage0r@icloud.com>
2024-01-18 16:58:36 +0100

    meilleuire voie,
    description de ce qu'il voit, détéecte mouvement, trop long quadrant 1, mettre des thresholds pour chaque, fine tuner et regarder de plus près le csv

--- Image_detection/test.py
+++ Image_detection/test.py
@@ -47,26 +47,27 @@ def split_frame_into_quadrants(frame, frame_width, frame_height):
 def process_quadrant(quadrant, background, contour_threshold):
     # Convert to grayscale and blur
     quadrant = cv2.cvtColor(quadrant, cv2.COLOR_BGR2GRAY)
-    quadrant = cv2.GaussianBlur(quadrant, (7, 7), 0)
+    # quadrant = cv2.GaussianBlur(quadrant, (7, 7), 0)
 
     if quadrant.shape[:2] != background.shape[:2]:
         raise ValueError("Quadrant and background should have the same dimensions")
 
     # Detect motion
     diff_frame = cv2.absdiff(quadrant, background)
-    _, thresh_frame = cv2.threshold(diff_frame, 3, 255, cv2.THRESH_BINARY)
+    _, thresh_frame = cv2.threshold(diff_frame, 50, 255, cv2.THRESH_BINARY)
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
 
     cnts, _ = cv2.findContours(thresh_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
         if cv2.contourArea(contour) >= contour_threshold:
+            print(f"Motion detected! Contour Area: {cv2.contourArea(contour)}")  # Debugging line
             return True  # Early termination if large contour is found
 
     return False  # No large contour found
 
-def find_dolphins_in(video_path, duration_threshold=2.0, contour_threshold=100, skip_frames=1):
-
+def find_dolphins_in(video_path, duration_threshold=2.0, contour_threshold=4000, skip_frames=1):
+    already_added_to_list = [False, False, False, False]
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print(f"Failed to read the video: {video_path}")
@@ -77,7 +78,7 @@ def find_dolphins_in(video_path, duration_threshold=2.0, contour_threshold=100, 
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    fourcc = cv2.VideoWriter_fourcc(*'x264')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     output_dir = os.path.join(os.path.dirname(video_path), "detected_dolphins")
     os.makedirs(output_dir, exist_ok=True)
 
@@ -99,71 +100,65 @@ def find_dolphins_in(video_path, duration_threshold=2.0, contour_threshold=100, 
     motion_in_frame = False
     frame_buffer = []  # Buffer to store frames
     motion_segment_started = False
+    already_added_to_list = [False, False, False, False]  # Track if motion data is already added to the list for each quadrant
+
     with tqdm(total=total_frames, desc=f"Processing Video {video_path}") as pbar:
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
+
+            motion_in_frame = False  # Reset motion flag for the current frame
             quadrants = split_frame_into_quadrants(frame, frame_width, frame_height)
-            # for each quadrants looks if there is a motion 
+
             for i, quadrant in enumerate(quadrants):
+                if i >= 2:
+                    break
                 motion_detected[i] = process_quadrant(quadrant, backgrounds[i], contour_threshold)
-                # if there is a motion the start time is set 
+
                 if motion_detected[i]:
                     if not motion_before[i]:
                         motion_times[i][0] = frame_count / fps
-                        motion_times[i][1] = frame_count / fps
                         motion_before[i] = True
-                    if motion_before[i]:
-                        motion_times[i][1] = frame_count / fps
-                else : 
-                    if motion_before[i]:
-                        motion_times[i][0] = 0
-                        motion_times[i][1] = 0
-                        motion_before[i] = False
-            
-
-                # Check if any quadrant has motion duration exceeding the threshold
-                for start, end in motion_times:
-                    if (end - start) >= duration_threshold:
+                    motion_times[i][1] = frame_count / fps
+                    current_duration = motion_times[i][1] - motion_times[i][0]
+                    print(f"Quadrant {i+1} Current Motion Duration: {current_duration} seconds")
+                elif motion_before[i]:
+                    duration = motion_times[i][1] - motion_times[i][0]
+                    if duration >= duration_threshold:
                         motion_in_frame = True
-                        break
+                        if not already_added_to_list[i]:
+                            rows_list.append({"Segment": len(rows_list) + 1, "Start": motion_times[i][0], "End": motion_times[i][1], "File": current_output_path, "Quadrant": i+1})
+                            already_added_to_list[i] = True
+                    motion_before[i] = False
+                    motion_times[i] = [0, 0]
+                    already_added_to_list[i] = False
 
-                if motion_in_frame:
-                    if not motion_segment_started:
-                        motion_segment_started = True
-                        frame_buffer = []  # Reset buffer
+            if motion_in_frame:
+                if not motion_segment_started:
+                    motion_segment_started = True
+                    frame_buffer = []
 
-                    frame_buffer.append(frame)  # Add frame to buffer
-                else:
-                    if motion_segment_started:
-                        # Motion segment ended, write buffer to file
-                        current_output_path = os.path.join(output_dir, f"segment_{frame_count}.mp4")
-                        video_writer = cv2.VideoWriter(current_output_path, fourcc, fps, (frame_width, frame_height))
+                frame_buffer.append(frame)
+            elif motion_segment_started:
+                # Save the buffered frames as a video segment
+                current_output_path = os.path.join(output_dir, f"segment_{frame_count}.mp4")
+                video_writer = cv2.VideoWriter(current_output_path, fourcc, fps, (frame_width, frame_height))
 
-                        for buffered_frame in frame_buffer:
-                            video_writer.write(buffered_frame)
+                for buffered_frame in frame_buffer:
+                    video_writer.write(buffered_frame)
 
-                        video_writer.release()
-                        video_writer = None
-
-                        # Append segment info to rows_list
-                        for start, end in motion_times:
-                            if (end - start) >= duration_threshold:
-                                rows_list.append({"Segment": len(rows_list) + 1, "Start": start, "End": end, "File": current_output_path})
-
-                        motion_times = [[0, 0] for _ in range(4)]  # Reset motion times
-                        motion_segment_started = False
+                video_writer.release()
+                video_writer = None
+                motion_segment_started = False
 
             frame_count += 1
             pbar.update(1)
 
-    if video_writer is not None:
-        video_writer.release()
+        if video_writer is not None:
+            video_writer.release()
 
     cap.release()
-
-    return pd.DataFrame(rows_list)
 
 
 def analyze_videos_in_folder(folder_path):
@@ -222,9 +217,9 @@ global_motion_df = analyze_videos_in_folder(folder_path)
 print(global_motion_df)
 
 
+# ValueError: bad marshal data (unknown type code)
+# # Replace with your .prof file path
+# prof_file_path = '/home/alexis/Desktop/video/testvideo_2min_profile_stats.prof'
 
-# Replace with your .prof file path
-prof_file_path = '/home/alexis/Desktop/video/testvideo_2min_profile_stats.prof'
-
-p = pstats.Stats(prof_file_path)
-p.sort_stats('cumulative').print_stats(10)  # Adjust the number to display more or fewer lines
+# p = pstats.Stats(prof_file_path)
+# p.sort_stats('cumulative').print_stats(10)  # Adjust the number to display more or fewer lines 
