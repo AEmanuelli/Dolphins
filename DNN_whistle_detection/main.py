6f28709c5f42e67c61623bf110b547a9ce9edc4c
Alexis Lab <accent.cartilage0r@icloud.com>
2024-03-19 16:33:23 +0100

    Update main.py

--- DNN_whistle_detection/main.py
+++ DNN_whistle_detection/main.py
@@ -10,12 +10,19 @@ if os.name == 'posix':  # Unix
 #     sys.stderr = open(os.devnull, 'w')
 
 if __name__ == "__main__":
-    
-    dossier_csv = "/media/DOLPHIN_ALEXIS1/Analyses_alexis/2023_analysed"  # Update with your actual path
+ 
     model_path = "models/model_vgg.h5"
-    recording_folder_path = "/media/DOLPHIN_ALEXIS1/2023"  # Update with your actual path
-    saving_folder = '/media/DOLPHIN_ALEXIS1/Analyses_alexis/2023_analysed'  # Update with your actual path
-    dossier_anciens_csv = "/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/predictions"  # Update with your actual path
+    # Sur PC Faadil 
+    recording_folder_path = "/media/DOLPHIN_ALEXIS1/2023"  
+    saving_folder = '/media/DOLPHIN_ALEXIS1/Analyses_alexis/2023_analysed' 
+    dossier_csv = "/media/DOLPHIN_ALEXIS1/Analyses_alexis/2023_analysed" 
+    
+    # Sur PC Alexis
+    # dossier_csv = "/media/DOLPHIN/Analyses_alexis/2023_analysed/"  
+    # recording_folder_path = "/media/DOLPHIN/2023/" 
+    # saving_folder = '/media/DOLPHIN/Analyses_alexis/2023_analysed/'
+
+    dossier_anciens_csv = "/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/predictions"  #plus à jour 
     process_predict_extract(recording_folder_path, saving_folder, start_time=0, 
                             end_time=1800, batch_size=75, save=False, save_p=True, 
                             model_path="models/model_vgg.h5", max_workers = 8)
