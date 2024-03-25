2386317e964e396d72573e4663cb5c3717c781fe
Alexis Lab <accent.cartilage0r@icloud.com>
2024-03-25 14:37:47 +0100

    Update main.py

--- DNN_whistle_detection/main.py
+++ DNN_whistle_detection/main.py
@@ -1,4 +1,4 @@
-# from process_predictions import process_prediction_files_in_folder
+from process_predictions import process_prediction_files_in_folder
 from predict_online_parallel import process_predict_extract
 import os 
 import sys
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     root = "/media/DOLPHIN_ALEXIS1/Analyses_alexis/2023_analysed" 
     
     # Sur PC Alexis
-    # dossier_csv = "/media/DOLPHIN/Analyses_alexis/2023_analysed/"  
+    # root = "/media/DOLPHIN/Analyses_alexis/2023_analysed/"  
     # recording_folder_path = "/media/DOLPHIN/2023/" 
     # saving_folder = '/media/DOLPHIN/Analyses_alexis/2023_analysed/'
 
     dossier_anciens_csv = "/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/predictions"  #plus à jour 
-    process_predict_extract(recording_folder_path, saving_folder, start_time=0, 
-                            end_time=1800, batch_size=75, save=False, save_p=True, 
-                            model_path="models/model_vgg.h5", max_workers = 8)
+    # # process_predict_extract(recording_folder_path, saving_folder, start_time=0, 
+    #                         end_time=1800, batch_size=75, save=False, save_p=True, 
+    #                         model_path="models/model_vgg.h5", max_workers = 8)
 
-    # process_prediction_files_in_folder(dossier_csv, recording_folder_path=root, max_workers = 16)
+    process_prediction_files_in_folder(root, recording_folder_path=recording_folder_path, max_workers = 16)
