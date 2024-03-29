1973b28174e929ab280e1ec40db41a78e08a1e58
AEmanuelli <accent.cartilage0r@icloud.com>
2024-03-29 14:11:52 +0100

    Update vidéoaudio.py

--- DNN_whistle_detection/vidéoaudio.py
+++ DNN_whistle_detection/vidéoaudio.py
@@ -1,92 +1,35 @@
-import cv2
+import os
+from moviepy.editor import VideoFileClip, AudioFileClip
+from tqdm import tqdm
 
-def inserer_images_sous_video(video_path, images, instants, duree, output_path):
-    """
-    Insère des images sous une vidéo aux instants spécifiés pour la durée spécifiée et enregistre la vidéo modifiée.
+def vidéoetaudio(intervalles, fichier_video, fichier_audio, dossier_sortie_video):
+    # Chargement de la vidéo et de l'audio
+    video = VideoFileClip(fichier_video)
+    audio = AudioFileClip(fichier_audio)
+    
+    # Calculer le nombre total d'extraits à générer
+    total_extraits = len(intervalles)
+    
+    # Afficher une barre de progression
+    with tqdm(total=total_extraits, desc=f'Extraction pour {fichier_video}') as pbar:
+        # Parcourir les intervalles
+        for i, intervalle in enumerate(intervalles):
+            debut, fin = intervalle
+            nom_sortie = f'extrait_{debut}_{fin}.mp4'  # Nom de sortie basé sur l'intervalle
 
-    Args:
-        video_path (str): Chemin vers la vidéo d'entrée.
-        images (list): Liste des chemins vers les images à insérer.
-        instants (list): Liste des instants (en secondes) pour chaque image.
-        duree (int): Durée (en secondes) pendant laquelle chaque image est affichée.
-        output_path (str): Chemin de sortie pour enregistrer la vidéo modifiée.
-    """
-
-    # Charger la vidéo
-    video_capture = cv2.VideoCapture(video_path)
-
-    # Définir le FPS (Frames Per Second) de la vidéo
-    fps = int(video_capture.get(cv2.CAP_PROP_FPS))
-
-    # Récupérer les dimensions de la vidéo
-    video_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    video_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # Calculer les nombres de frames pour chaque instant
-    instants_frames = [int(instant * fps) for instant in instants]
-
-    # Lire les images
-    images_loaded = [cv2.imread(image) for image in images]
-
-    # Récupérer les dimensions des images
-    image_heights, image_widths = zip(*[image.shape[:2] for image in images_loaded])
-
-    # Calculer la hauteur maximale parmi les images
-    max_height = max(image_heights)
-
-    # Définir le temps écoulé en secondes
-    temps_ecoule = 0
-
-    # Indice de l'image à afficher
-    current_image_index = 0
-
-    # Créer un objet VideoWriter pour enregistrer la vidéo modifiée
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Format de compression vidéo
-    output_video = cv2.VideoWriter(output_path, fourcc, fps, (video_width, video_height))
-
-    # Lire la vidéo frame par frame
-    while video_capture.isOpened():
-        ret, frame = video_capture.read()
-
-        if not ret:
-            break
-
-        # Vérifier si l'instant actuel correspond à l'instant d'une image
-        if temps_ecoule in instants_frames:
-            # Insérer l'image sous la vidéo
-            frame[0:max_height, 0:image_widths[current_image_index]] = images_loaded[current_image_index]
-
-            # Calculer le décalage pour la prochaine image si les instants des images sont successifs
-            if current_image_index < len(images_loaded) - 1 and instants_frames[current_image_index] == instants_frames[current_image_index + 1]:
-                offset = image_widths[current_image_index]
-            else:
-                offset = 0
-
-            # Mettre à jour l'indice de l'image à afficher
-            current_image_index += 1
-
-        # Écrire le frame modifié dans la vidéo de sortie
-        output_video.write(frame)
-
-        # Incrémenter le temps écoulé
-        temps_ecoule += 1
-
-        # Vérifier si la durée d'affichage de l'image est écoulée
-        if temps_ecoule in [instant + duree * fps for instant in instants_frames]:
-            # Réinitialiser l'indice de l'image à afficher
-            current_image_index = 0
-
-    # Libérer la capture vidéo et fermer les fenêtres OpenCV
-    video_capture.release()
-    output_video.release()
-    cv2.destroyAllWindows()
-
-# Exemple d'utilisation
-video_path = '/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/corbeille/extraits/Exp_19_Jul_2023_1445_channel_1.wav/extrait_0_10.mp4'
-images = ["/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/2023/Exp_31_May_2023_1245_channel_1/positive/220.4-220.8.jpg",
-           "/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/2023/Exp_31_May_2023_1245_channel_1/positive/220.4-220.8.jpg"]
-instants = [1, 4]  # Instants en secondes pour chaque image
-duree = 3  # Durée en secondes pendant laquelle chaque image est affichée
-
-# Appel de la fonction pour insérer les images sous la vidéo
-inserer_images_sous_video(video_path, images, instants, duree, video_path)
+            chemin_sortie = os.path.join(dossier_sortie_video, nom_sortie)  # Chemin complet de sortie
+            if not os.path.exists(chemin_sortie):
+                # Extraire l'extrait correspondant à l'intervalle
+                extrait_video = video.subclip(debut, fin)
+                extrait_audio = audio.subclip(debut, fin)
+                # Ajouter l'audio à la vidéo extraite
+                extrait_video = extrait_video.set_audio(extrait_audio)
+                # Sauvegarder l'extrait vidéo avec audio
+                extrait_video.write_videofile(chemin_sortie, codec='libx264', audio_codec='aac', fps=video.fps, verbose=False)
+            else : 
+                print("zbzbz")
+            pbar.update(1)
+        
+    # Libérer la mémoire en supprimant les objets VideoFileClip et AudioFileClip
+    video.close()
+    audio.close()
