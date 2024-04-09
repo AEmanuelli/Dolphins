f705625e6a49ffbffcddf137eb16d21ea91eb3d2
AEmanuelli <accent.cartilage0r@icloud.com>
2024-04-09 15:59:33 +0200

    final

--- DNN_whistle_detection/Predict_and_extract/main.py
+++ DNN_whistle_detection/Predict_and_extract/main.py
@@ -7,7 +7,7 @@ import sys
 # #     sys.stderr = open(os.devnull, 'w')
 
 from process_predictions import process_prediction_files_in_folder
-from predict_and_extract_online import process_predict_extract
+# from predict_and_extract_online import process_predict_extract
 
 
 if __name__ == "__main__":  
@@ -17,19 +17,19 @@ if __name__ == "__main__":
     # Sur PC Faadil 
     # from predict_online_parallel import process_predict_extract
 
-    recordings = "/media/DOLPHIN_ALEXIS/2023"  
-    saving_folder = '/media/DOLPHIN_ALEXIS/Analyses_alexis/2023_analysed' 
-    root = "/media/DOLPHIN_ALEXIS/Analyses_alexis/2023_analysed" 
+    # recordings = "/media/DOLPHIN_ALEXIS/2023"  
+    # saving_folder = '/media/DOLPHIN_ALEXIS/Analyses_alexis/2023_analysed' 
+    # root = "/media/DOLPHIN_ALEXIS/Analyses_alexis/2023_analysed" 
     
     # # Sur PC Alexis
 
-    # root = "/media/DOLPHIN/Analyses_alexis/2023_analysed/"  
-    # recordings = "/media/DOLPHIN/2023/" 
-    # saving_folder = '/media/DOLPHIN/Analyses_alexis/2023_analysed/'
+    root = "/media/DOLPHIN/Analyses_alexis/2023_analysed/"  
+    recordings = "/media/DOLPHIN/2023/" 
+    saving_folder = '/media/DOLPHIN/Analyses_alexis/2023_analysed/'
 
     dossier_anciens_csv = "/users/zfne/emanuell/Documents/GitHub/Dolphins/DNN_whistle_detection/predictions"  #plus à jour 
-    process_predict_extract(recordings, saving_folder, start_time=0, 
-                            end_time=1800, batch_size=75, save=False, save_p=True, 
-                            model_path="DNN_whistle_detection/models/model_vgg.h5", max_workers = 8)
+    # process_predict_extract(recordings, saving_folder, start_time=0, 
+    #                         end_time=1800, batch_size=75, save=False, save_p=True, 
+    #                         model_path="DNN_whistle_detection/models/model_vgg.h5", max_workers = 8)
 
     process_prediction_files_in_folder(root, recording_folder_path=recordings, max_workers = 10)
